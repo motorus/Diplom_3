cab989e1750fbf33454365052ea6f3d498255b63
motorus <CTPEJIOK1979@yandex.ru>
2024-05-22 18:57:37 +0500

    Убрал локаторы из тестов

--- tests/test_account_page.py
+++ tests/test_account_page.py
@@ -8,17 +8,14 @@ class TestAccount:
     @allure.title("Проверка нажатия на кнопку 'История заказов'")
     def test_click_to_order_history(self, driver, logged_driver):
         account_page = AccountPage(driver)
-
         assert account_page.click_to_order_history() == URLS.ORDER_HISTORY
 
     @allure.title("Проверка нажатия на кнопку 'Выход'")
     def test_click_to_exit(self, driver, logged_driver):
         account_page = AccountPage(driver)
-
         assert account_page.click_to_exit_button() == URLS.LOGIN
 
     @allure.title("Проверка нажатия на кнопку 'Конструктор'")
     def test_click_to_constructor(self, driver, logged_driver):
         account_page = AccountPage(driver)
-
         assert account_page.click_to_constructor_button() == URLS.MAIN
