8e9bc25a13f85b3bafc08a97b2a881a4c013d190
motorus <CTPEJIOK1979@yandex.ru>
2024-05-22 14:18:13 +0500

    удалил использование локаторов

--- pages/account_page.py
+++ pages/account_page.py
@@ -8,16 +8,20 @@ class AccountPage(BasePage):
 
     @allure.step("Клик по истории заказов в личном кабинете")
     def click_to_order_history(self):
+        self.open_page(URLS.PERSONAL_AREA)
         self.click_to_element(AccountLocators.ORDER_HISTORY)
         return self.return_page_url()
 
     @allure.step("Клик по кнопке выход в личном кабинете")
     def click_to_exit_button(self):
+        self.open_page(URLS.PERSONAL_AREA)
         self.click_to_element(AccountLocators.EXIT_BUTTON)
         self.wait_for_load_window(URLS.LOGIN)
         return self.return_page_url()
 
     @allure.step("Клик по кнопке конструктор")
     def click_to_constructor_button(self):
+        self.open_page(URLS.PERSONAL_AREA)
         self.click_to_element(AccountLocators.CONSTRUCTOR_BUTTON)
         return self.return_page_url()
+
